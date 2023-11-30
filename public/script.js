e18ee88174211aa7ec4fb75e9a817da349e35b77
Taylor the Diabetic <107991832+TheWiredDiabetic@users.noreply.github.com>
2023-11-30 20:54:55 +0000

    Phase 2 - Overhall of most files, added public directory with other files and updated stylesheets and scripts.

---
+++ public/script.js
@@ -0,0 +1,41 @@
+let socket;
+let username;
+
+const isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+if (isDarkMode) {
+  $('body').addClass('dark-mode');
+}
+
+function toggleDarkMode() {
+  const isDarkModeEnabled = $('body').hasClass('dark-mode');
+  $('body').toggleClass('dark-mode');
+  $('#dark-mode-toggle').parent().toggleClass('dark-mode-toggle-on');
+  localStorage.setItem('darkMode', !isDarkModeEnabled);
+}
+
+function joinChat() {
+  username = $('#username').val();
+  if (username.trim() !== '') {
+    socket = io();
+    socket.emit('join', username);
+
+    $('#username-container').addClass('hide');
+    $('#chat-container').removeClass('hide');
+
+    $('#form').submit(function () {
+      const message = $('#m').val();
+      if (message.trim() !== '') {
+        socket.emit('chat message', { username, message });
+        $('#m').val('');
+      }
+      return false;
+    });
+
+    socket.on('chat message', function (data) {
+      const messageContainer = $('#messages');
+      messageContainer.append(`<div class="${data.type}"><strong>${data.username}:</strong> ${data.message}</div>`);
+      messageContainer.scrollTop(messageContainer[0].scrollHeight);
+    });
+  }
+}
